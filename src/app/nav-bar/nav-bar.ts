b375758f09e7970ff2317f92ea0a582c5a70ea54
Connie <138333893+conniechen651@users.noreply.github.com>
2025-09-22 16:30:47 -0700

    Added navbar and material for UI

---
+++ src/app/nav-bar/nav-bar.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatIcon, MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [RouterLink, 
+            MatToolbarModule, 
+            MatIconModule, 
+            MatButtonModule],
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.scss'
+})
+export class NavBar {
+
+}
