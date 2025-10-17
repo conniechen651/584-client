e4c23c38c1ba12443d0a3436a0fa3b4da90f5874
Connie <138333893+conniechen651@users.noreply.github.com>
2025-10-16 20:54:23 -0700

    Add school and district components, update nav bar links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { School } from './school/school';
+import { District } from './district/district';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home, pathMatch: 'full'},
+    {path: 'school', component: School}, 
+    {path: 'district', component: District}, 
+];
