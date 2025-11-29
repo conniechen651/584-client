df30448dbaeebca654520408d854922c23bece26
Connie <138333893+conniechen651@users.noreply.github.com>
2025-11-28 20:40:24 -0800

    change environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'comp584server-h6g8b9bqdwfye8ab.canadacentral-01.azurewebsites.net',
+    apiUrl: 'https://comp584server-h6g8b9bqdwfye8ab.canadacentral-01.azurewebsites.net',
     auth0: {
         domain: 'dev-yl7blimqv18gp6tv.us.auth0.com',
         clientId: 'xcNuAFIjAK3Yzpq1rzLnBmtFtoxdGGgB'
