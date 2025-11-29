dc635928803e7d763acf38c1fff291a33edc17d3
Connie <138333893+conniechen651@users.noreply.github.com>
2025-11-28 20:47:58 -0800

    Testing build environment in production

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from "./nav-bar/nav-bar";
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,7 @@ import { NavBar } from "./nav-bar/nav-bar";
 })
 export class App {
   protected readonly title = signal('584-client');
+  constructor() {
+    console.log('BUILD ENV:', environment); // will show apiUrl used at build time
+  }
 }
