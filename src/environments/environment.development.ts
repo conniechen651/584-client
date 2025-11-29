8561ce2418619d28215b2737b8c66b0695a2def7
Connie <138333893+conniechen651@users.noreply.github.com>
2025-11-28 20:50:51 -0800

    Testing api url in other environment file

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5089',
+    apiUrl: 'https://comp584server-h6g8b9bqdwfye8ab.canadacentral-01.azurewebsites.net',
     auth0: {
         domain: 'dev-yl7blimqv18gp6tv.us.auth0.com',
         clientId: 'xcNuAFIjAK3Yzpq1rzLnBmtFtoxdGGgB'
