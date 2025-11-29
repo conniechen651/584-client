bd4fc10c0ab4cbe3cac7d85ff599738c66d099dc
Connie <138333893+conniechen651@users.noreply.github.com>
2025-11-28 21:50:52 -0800

    Remove debugging code

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://comp584server-h6g8b9bqdwfye8ab.canadacentral-01.azurewebsites.net',
+    apiUrl: 'http://localhost:5089',
     auth0: {
         domain: 'dev-yl7blimqv18gp6tv.us.auth0.com',
         clientId: 'xcNuAFIjAK3Yzpq1rzLnBmtFtoxdGGgB'
